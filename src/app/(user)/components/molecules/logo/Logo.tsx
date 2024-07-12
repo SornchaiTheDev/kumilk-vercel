aad478ebdb169140c0dc71b7d5008d46c2f213c0
Teerut26 <teerutsr@gmail.com>
2024-07-13 00:06:39 +0700

    chore: Update logo text to include Thai translation

--- src/app/(user)/components/molecules/logo/Logo.tsx
+++ src/app/(user)/components/molecules/logo/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 cursor-pointer">
       <img className="h-8" src="/logo.webp" alt="" />
-      <div className="font-bold">Shop KU Milk</div>
+      <div className="font-bold">KU Milk | โรงนมเกษตร</div>
     </Link>
   );
 }
