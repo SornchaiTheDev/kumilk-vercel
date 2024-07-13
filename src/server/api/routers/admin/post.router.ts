679a7fca443a0948ce8b09b68f75f2f23c912427
SornchaiTheDev <sornchaithedev@gmail.com>
2024-07-13 17:57:40 +0700

    chore: removed unused code

--- src/server/api/routers/admin/post.router.ts
+++
@@ -1,8 +0,0 @@
-import { loginSchema } from "@/schemas/login";
-import { publicProcedure, router } from "@/server/api/trpc";
-
-export const postRouter = router({
-  login: publicProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
-    return "OK";
-  }),
-});
